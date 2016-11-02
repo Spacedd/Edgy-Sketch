96fe8df77741f86c781ff76ec9a515c5b4398a83
Daniel Hammond <daniel@jammyweb.org.uk>
2016-11-02 16:37:00 +0000

    Canvas is working properly
    Commented everything
    
    Altered messages so it is unordered list as users said they didn't like the bullet points

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ $(function(){
 // Initialize variables
     var $window = $(window);
     var $usernameInput = $('.usernameInput'); // Input for username
-   // var $messages = $('.messages'); // Messages area
+    // var $messages = $('.messages'); // Messages area
     var $inputMessage = $('.inputMessage'); // Input message input box
 
     var $loginPage = $('.login.page'); // The login page
@@ -17,48 +17,69 @@ $(function(){
     var lastTypingTime;
     var $currentInput = $usernameInput.focus();
 
+// Creates the canvas and gives it the element draw
     var c = document.getElementById("draw");
     var ctx = c.getContext("2d");
 
-    var pen = "?";
-    var dXpos;
-    var dYpos;
-    var uXpos;
-    var uYpos;
+// Variables for the canvas and pen
+    var startX;
+    var startY;
+    var endX;
+    var endY;
+    var old = {x:0 , y:0};
 
+// States what the pen will draw
+    ctx.fillStyle = "solid";
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
 
+// Adds the listeners for the canvas for when the mouse goes up, down or just moves
     c.addEventListener("mousedown", doMouseDown, false);
     c.addEventListener("mouseup", doMouseUp, false);
     c.addEventListener("mousemove", doMouseMove, false);
 
+// Variable which later decides whether to draw or not
+    var ismousedown = false;
+
+// Grabs the x and y coordinates of the mousedown position
     function doMouseDown(event){
-        dXpos = event.pageX - ctx.canvas.offsetLeft;
-        dYpos = event.pageY - ctx.canvas.offsetTop;
-        socket.emit('draw', {xDown: dXpos, yDown: dYpos})
-        pen = "down";
+        ismousedown = true;
+        startX = event.pageX - ctx.canvas.offsetLeft;
+        startY = event.pageY - ctx.canvas.offsetTop;
+
     }
 
+// Grabs the x and y coordinates of the mouseup position and then sends an emit to the function draw
     function doMouseUp(event){
-        uXpos = event.pageX - ctx.canvas.offsetLeft;
-        uYpos = event.pageY - ctx.canvas.offsetTop;
-        socket.emit('draw', {xDown: dXpos, yDown: dYpos, xUp: uXpos, yUp: uYpos});
-        pen = "up";
-    }
+        ismousedown = false;
+        endX = event.pageX - ctx.canvas.offsetLeft;
+        endY = event.pageY - ctx.canvas.offsetTop;
+        socket.emit('draw', {endX: endX, endY: endY, startX: startX, startY: startY});}
 
+// Allows the pen to draw freely, not in straight lines, by setting the position it has just been as the
+// beginning of the "line"
     function doMouseMove(event){
-
+        if (ismousedown) {
+            endX = event.pageX - ctx.canvas.offsetLeft;
+            endY = event.pageY - ctx.canvas.offsetTop;
+            socket.emit('draw', {endX: endX, endY: endY, startX: startX, startY: startY});
+            startX = endX;
+            startY = endY;
+        }
     }
 
+// Actually draws the line with the function 'stroke', moves to the start coordinates and draws a line to the end ones
+// and sets the old variable to the current x and y coordinates, so that it can run again
     socket.on("draw", function(msg){
         ctx.beginPath();
-        ctx.moveTo(msg.xDown, msg.yDown);
-        ctx.lineTo(msg.xUp, msg.yUp);
+        ctx.moveTo(msg.startX, msg.startY);
+        ctx.lineTo(msg.endX, msg.endY);
         ctx.stroke();
-
-
+        old = msg;
     });
 
-    $('form').submit(function(){
+// Validates the message, then sets the contents to blank after it has been sent
+   $('form').submit(function(){
         if ($('#m').val() !== "") {
             socket.emit('chat message', $('#m').val());
             $('#m').val('');
@@ -66,8 +87,9 @@ $(function(){
         return false;
     });
 
+// Outputs the latest message
     socket.on('chat message', function(msg) {
-        $('#messages').append($('<li>').text(msg))
+        $('#messages').append($('<ul>').text(msg))
     });
 
 
