c94e8a5cfc3ce857add75d3372038c672eaa6359
Daniel Hammond <daniel@jammyweb.org.uk>
2017-02-08 22:03:09 +0000

    Refactoring things.

--- client/game.js
+++ client/game.js
@@ -1,20 +1,23 @@
 $(function () {
-    var socket = io();   
+
+                        // INITIALIZING VARIABLES //
+    
+
+    var socket = io();
 
     // Creates the canvas and gives it the element draw
     var c = document.getElementById("draw");
     var ctx = c.getContext("2d");
 
     // Variables for the canvas and pen
-    var startX;
-    var startY;
-    var endX;
-    var endY;
-    var old = {x: 0, y: 0};
-
+    var startPos;
+    var endPos;
     var thickness = 5;
     var colour = "#000000";
 
+
+                            // CANVAS EVENTS //
+    
 
     // States what the pen will draw
     ctx.fillStyle = "solid";
@@ -26,53 +29,41 @@ $(function () {
     c.addEventListener("mouseup", doMouseUp, false);
     c.addEventListener("mousemove", doMouseMove, false);
     c.addEventListener("mouseout", onMouseOut, false);
-    c.addEventListener("getrect", getRect, false);
-
+    c.addEventListener("getrect", getPos, false);
 
     // Variable which later decides whether to draw or not
     var ismousedown = false;
 
-    
-    // Sets coordinates based on the relative size of the canvas
-    function getRect(event){
-      var rect = c.getBoundingClientRect();
-      var width = (rect.right - rect.left);
-      var height = (rect.bottom - rect.top);
-      var x = event.clientX - rect.left;
-      var y = event.clientY - rect.top;
-      return {w: width, h: height, x: x, y: y};
+    function getPos(event){
+        var rect = c.getBoundingClientRect();
+        var width = (rect.right - rect.left);
+        var height = (rect.bottom - rect.top);
+        var originalX = event.clientX - rect.left;
+        var originalY = event.clientY - rect.top;
+        var x = originalX * (c.width/width);
+        var y = originalY * (c.height/height);
+        return {x: x, y: y};
     }
-
 
     // Grabs the x and y coordinates of the mousedown position
     function doMouseDown(event){
-        var rect = getRect(event);
-        startX = rect.x * (c.width/rect.w);
-        startY = rect.y * (c.height/rect.h);
+        startPos = getPos(event);
         ismousedown = true;
     }
-
 
     // Grabs the x and y coordinates of the mouseup position and then sends an emit to the function draw
     function doMouseUp(event){
         ismousedown = false;
-        var rect = getRect(event);
-        endX = rect.x * (c.width/rect.w);
-        endY = rect.y * (c.height/rect.h);
-        socket.emit('draw', {endX: endX, endY: endY, startX: startX, startY: startY, colour: colour, thickness: thickness, mouse: "up"});
+        endPos = getPos(event);
+        socket.emit('draw', {end: endPos, start: startPos, colour: colour, thickness: thickness, mouse: "up"});
     }
 
-
-    // Allows the pen to draw freely, not in straight lines, by setting the position it has just been as the
-    // beginning of the "line"
+    // Allows the pen to draw freely, by setting the position it has just been as the beginning of the "line"
     function doMouseMove(event){
         if (ismousedown){
-            var rect = getRect(event);
-            endX = rect.x * (c.width/rect.w);
-            endY = rect.y * (c.height/rect.h);
-            socket.emit('draw', {endX: endX, endY: endY, startX: startX, startY: startY, colour: colour, thickness: thickness, mouse: "down"});
-            startX = endX;
-            startY = endY;
+            endPos = getPos(event);
+            socket.emit('draw', {end: endPos, start: startPos, colour: colour, thickness: thickness, mouse: "down"});
+            startPos = endPos;
         }
     }
 
@@ -81,18 +72,11 @@ $(function () {
     }
 
 
-    // When erase is selected the pen is set to white, the thickness is corrected and the sliders are disabled
-    $("#erases").on('click', function() {
-        colour = "#ffffff";
-        thickness = refreshSize();
-        $("#sliderR, #sliderG, #sliderB").slider({
-            disabled : true
-        });
-    });
-
+                            // DRAWING OPTIONS //
+    
 
     // When the pen is selected the colour is refreshed, the thickness is corrected and the sliders are enabled
-    $("#penmode").on('click', function() {
+    $("#penmode").on('click', function(){
         colour = refreshColour();
         thickness = refreshSize();
         $('#sliderR, #sliderG, #sliderB').slider({
@@ -100,6 +84,26 @@ $(function () {
         });
     });
 
+    // When erase is selected the pen is set to white, the thickness is corrected and the sliders are disabled
+    $("#erases").on('click', function(){
+        colour = "#ffffff";
+        thickness = refreshSize();
+        $("#sliderR, #sliderG, #sliderB").slider({
+            disabled : true
+        });
+    });
+
+    // Makes sure only one box can be ticked at a time from array of checkboxes
+    $('input:checkbox').on('click', function(){
+        var $box = $(this);
+        if ($box.is(":checked")) {
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            $(group).prop("checked", false);
+            $box.prop("checked", true);
+        } else {
+            $box.prop("checked", false);
+        }
+    });
 
     // The slider class for the thickness slider
     $("#sliderPen").slider({
@@ -111,24 +115,31 @@ $(function () {
         change: refreshSize
     });
 
+    // Sets the rgb sliders to a min and max
+    $("#sliderR, #sliderG, #sliderB").slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 1,
+        slide: refreshColour,
+        change: refreshColour
+    });
+
+    // Returns the colour of the hex value generated from the rgb sliders, also displays the colour in the display box
+    function refreshColour(){
+        var red = $("#sliderR").slider("value"),
+        green = $("#sliderG").slider("value"),
+        blue = $("#sliderB").slider("value"),
+        hex = hexFromRGB(red, green, blue);
+        colour = "#" + hex;
+        $("#colourDisplay").css("background-color", colour);
+        return colour
+    }
 
     // Returns the value of the thickness slider into the pen sizes
     function refreshSize(){
        thickness = $("#sliderPen").slider("value");
     }
-
-
-    // Returns the colour of the hex value generated from the rgb sliders, also displays the colour in the display box
-    function refreshColour() {
-        var red = $("#sliderR").slider("value"),
-            green = $("#sliderG").slider("value"),
-            blue = $("#sliderB").slider("value"),
-            hex = hexFromRGB(red, green, blue);
-            colour = "#" + hex;
-        $("#colourDisplay").css("background-color", colour);
-        return colour
-    }
-
 
     // Converts the binary value to hex for each slider
     function hexFromRGB(r, g, b) {
@@ -145,59 +156,6 @@ $(function () {
         return hex.join("").toUpperCase();  // Joins the hex values to one string
     }
 
-
-    // Sets the rgb sliders to a min and max
-    $("#sliderR, #sliderG, #sliderB").slider({
-        orientation: "horizontal",
-        range: "min",
-        max: 255,
-        value: 1,
-        slide: refreshColour,
-        change: refreshColour
-    });
-
-
-    socket.on("connect", function(){
-        socket.emit("Newuser", username)
-    });
-
-    socket.on("disconnect", function(){
-        socket.emit("disconnect", username)
-    });
-
-
-    socket.on("users", function(users){
-        var messageArea = $('#userlist');
-        messageArea.empty();
-        for (i = 0; i < users.length; i ++){
-           if (users[i]){
-               var message = document.createElement('li');
-               message.setAttribute('class', 'users');
-               message.innerHTML = users[i].name + " [" + users[i].score + "]";
-               messageArea.append(message);
-               message.scrollIntoView(false);
-           }
-       }
-    });
-
-
-    // Draws the line with the function 'stroke', moves to the start coordinates and draws a line to the end ones
-    // and sets the old variable to the current x and y coordinates, so that it can run again
-    socket.on("draw", function (msg) {
-        ctx.beginPath();
-        ctx.moveTo(msg.startX, msg.startY);
-        ctx.lineTo(msg.endX, msg.endY);
-        ctx.strokeStyle = msg.colour;
-        ctx.lineWidth = msg.thickness;
-        ctx.stroke();
-        old = msg;
-    });
-    
-    socket.on('keepAlive', function(){
-        socket.emit('keepAlive');
-    });
-
-
     //  Replaces the canvas with a white rectangle, aka clearing it
     ctx.clear = function () {
         ctx.clearRect(0, 0, c.width, c.height);
@@ -208,67 +166,113 @@ $(function () {
         socket.emit('clear');
     });
 
-    // Calls the clear function when it receives the call from the server
-    socket.on('clear', function () {
-        ctx.clear();
-    });
+    
+                            // MESSAGING OPTIONS //
+    
 
+    // The message is passed through as a string and this outputs it into the messagearea
+    function messageCreation(text, property){
+        var message = document.createElement('div');
+       /*var colour;
+        if(property === "server") {
+            colour = "red";
+        } else {
+            colour = "black";
+        }*/        
+        //text = "<span style = 'color:" + colour + ";'>" + text + "</span>";
+        message.setAttribute('class', property);
+        message.innerHTML = text;
+        var messageArea = $('.chat-messages');
+        messageArea.append(message);
+        message.scrollIntoView(false);
+    }
 
     // Validates the message, then sets the contents to blank after it has been sent
     $('#sendmessage').submit(function () {
         if ($('#m').val() !== "") {
-            socket.emit('chat message', username + " :" +  $('#m').val());
+            socket.emit('chat message', username + " :" +  $('#m').val(), "client");
             $('#m').val('');
         }
         return false;
     });
 
-
     // When the user selects their nickname it emits that the user has changed their name, and their new nickname.
-    // Also validates if the nickname box is empty
     $('#setnick').submit(function () {
-        if ($('#nick').val() !== "" && $('#nick').val().length < 20) {                //if the nickname is greater than 20 or empty
-            var newusername = $("#nick").val();
-            socket.emit('chat message', username + " changed their nickname to: " + newusername);
+        var re = new RegExp("^[a-zA-Z]{1,20}$"); //Only allows characters between a-z lower and uppercase, and only 1-20 of them
+        var newusername = $("#nick").val();
+        if (re.test(newusername)){
+            socket.emit('chat message', username + " changed their nickname to: " + newusername, "server");
             socket.emit('nickname', newusername);
             username = newusername;
             $('#nick').val('');
         } else {
-
-        }
+            messageCreation("Enter a valid nickname - Only UPPER and lower case letters", "server");
+            $('#nick').val('');
+        };
         return false;
     });
+    
+    
+                    // SOCKETS //
 
+    
+    socket.on("connect", function(){
+        socket.emit("Newuser", username)
+    });
+
+    // Adds the connected user to the list, or if game is running, it is used to updates the users' scores
+    socket.on("users", function(users){
+        var messageArea = $('#userlist');
+        messageArea.empty();
+        for (i = 0; i < users.length; i ++){
+            if (users[i]){
+                var message = document.createElement('li');
+                message.setAttribute('class', 'users');
+                message.innerHTML = users[i].name + " [" + users[i].score + "]";
+                messageArea.append(message);
+                message.scrollIntoView(false);
+            }
+        }
+    });
 
     // Outputs the latest message
-    socket.on('chat message', function (msg) {
-        messageCreation(msg)
+    socket.on('chat message', function(msg, property){
+        messageCreation(msg, property);  // The property defines whether the message is coming from the client or server
     });
 
+    // Draws the line with the function 'stroke', moves to the startPos coordinates and draws a line to the endPos ones
+    // and sets the old variable to the current x and y coordinates, so that it can run again
+    socket.on("draw", function(msg){
+        ctx.beginPath();
+        ctx.moveTo(msg.start.x, msg.start.y);
+        ctx.lineTo(msg.end.x, msg.end.y);
+        ctx.strokeStyle = msg.colour;
+        ctx.lineWidth = msg.thickness;
+        ctx.stroke();
+    });
 
-    // Makes sure only one box can be ticked at a time from array of checkboxes
-    $('input:checkbox').on('click', function () {
-        var $box = $(this);
-        if ($box.is(":checked")) {
-            var group = "input:checkbox[name='" + $box.attr("name") + "']";
-            $(group).prop("checked", false);
-            $box.prop("checked", true);
-        } else {
-            $box.prop("checked", false);
+    // Calls the clear function when it receives the call from the server
+    socket.on('clear', function () {
+        ctx.clear();
+    });
+
+    socket.on('game startPos', function(round, currentWordLength){
+        if (round === 1) {
+            socket.emit('chat message', "The game is afoot", "server");
+        }else{
+            socket.emit('chat message', "Round : " + round, "server");
         }
-    });
-    
-
-    $('#startGame').on('click', function(){
-        var wordSet = $("#wordSetDropBox").val();
-        if (wordSet !== "Default"){
-            socket.emit('newgame', wordSet);
-            socket.emit('clear');
-        } else {
-            alert("Select a wordset before starting the game!");
-        }
+        socket.emit('clear');
+        document.getElementById('wordDisplay').innerHTML = "Word : " + wordToUnderscore(currentWordLength);
     });
 
+    socket.on('current word', function(wordToGuess){
+        document.getElementById('wordDisplay').innerHTML = ("Draw the word: " + wordToGuess);
+    });
+
+    socket.on('displayTime', function(msg){
+        document.getElementById('displayTime').innerHTML = msg;
+    });
 
     socket.on('hidebox', function(){
         document.getElementById("startGame").disabled = true;
@@ -280,22 +284,25 @@ $(function () {
         document.getElementById('wordDisplay').innerHTML = "";
     });
 
-
-   socket.on('current word', function(wordToGuess){
-        document.getElementById('wordDisplay').innerHTML = ("Draw the word: " + wordToGuess);
+    socket.on('globalWordToShow', function(msg){
+        document.getElementById('wordDisplay').innerHTML = "The word was '" + msg + "'";
     });
 
-    socket.on('game start', function(round, currentWordLength){
-        if (round === 1) {
-            messageCreation("The game is afoot");
-        }else{
-            messageCreation("Round : " + round);
+
+                            // GAME OPTIONS //
+    
+
+    $('#startGame').on('click', function(){
+        var wordSet = $("#wordSetDropBox").val();
+        if (wordSet !== "Default"){
+            socket.emit('newgame', wordSet);
+            socket.emit('clear');
+        } else {
+            messageCreation("Select a wordset before starting the game!", "server");
         }
-        socket.emit('clear');
-        document.getElementById('wordDisplay').innerHTML = "Word : " + wordToUnderscore(currentWordLength);
     });
 
-
+    // Converts the word to guess into underscores "_" for the guessers to see how long the word is
     function wordToUnderscore(currentWordLength){
         var underscore = "";
         for (var i = 0; i < currentWordLength; i ++){
@@ -304,28 +311,8 @@ $(function () {
         return underscore;
     }
 
-
-    function messageCreation(text){
-        var message = document.createElement('div');
-        message.setAttribute('class', 'chat-message');
-        message.innerHTML = text;
-        var messageArea = $('.chat-messages');
-        messageArea.append(message);
-        message.scrollIntoView(false);
-    }
-
-    socket.on('displayTime', function(msg){
-        document.getElementById('displayTime').innerHTML = msg;
-    });
-
-
-    socket.on('globalWordToShow', function(msg){
-        document.getElementById('wordDisplay').innerHTML = "The word was '" + msg + "'";
-    });
-
-
-    function ROOM(){
-    /*   ___      ___
+/*              ROOM
+     ___      ___
     /   \____/   \
    /    / __ \    \
   /    |  ..  |    \
@@ -337,6 +324,6 @@ $(function () {
      | @ |  | @ || @ |   '
      |   |~~|   ||   |
      'ooo'  'ooo''ooo'
-    */
-    };
+
+*/    
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+					// VARIABLE INITIALIZATION //
+
 //loads the packages needed
 
 var express = require('express');
@@ -6,39 +8,36 @@ var http = require('http').Server(app);
 var favicon = require('serve-favicon');
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {'pingInterval': 1000, 'pingTimeout': 3000});
 var mustacheExpress	= require('mustache-express');
-var users = new Array();
+var users = [];
 var database = require ("./database.js");
 
 var game = {
-	gameWords: new Array(),
+	gameWords: [],
 	currentWord: "",
 	round: 0,
 	drawer: null,
 	gamerunning: false,
-	startCount: 5,
+	startCount: 30,
 	count: 0,
 	t: 0,
 	timerOn: 0
 };
 
 // Says what port to listen to incoming data on
-
 const PORT=80;
 
 // Enables the page logo
-
 app.use(favicon(__dirname + "/../client/media/pen logo.png"));
 
 
 // Setup Facebook login strategy
-
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
 
 
-// if Facebook env vars not set - then use blanks
+// If the Facebook environment variables are not set, then use blanks
 if(clientId == null) {
 	clientId = "blankId";
 }
@@ -47,6 +46,7 @@ if(clientSecret == null) {
 	console.log(clientSecret);
 }
 
+// On returning from the Facebook login, redirect to the main page with the username extracted from the profile
 passport.use(new Strategy({
 	clientID: clientId,
 	clientSecret: clientSecret,
@@ -60,7 +60,6 @@ passport.use(new Strategy({
 
 
 // Configure Passport authenticated session persistence
-
 passport.serializeUser(function(user, cb) {
 	cb(null, user);
 });
@@ -70,8 +69,7 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 
-// Use application-level middleware for common functionality, including logging, parsing, and session handling.
-
+// Use application-level middleware for common functionality, including logging, parsing, and session handling
 app.use(express.static('client'));
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
@@ -79,7 +77,6 @@ app.use(require('cookie-parser')());
 app.use(require('express-session')({ secret: 'edgysketch', resave: true, saveUninitialized: true }));
 
 // Initialize Passport and restore authentication state, if any, from the session.
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -90,13 +87,11 @@ app.set('views', __dirname);
 // Uses app to send the request to the file login.html
 
  app.get('/', function(req,res) {
-	//res.sendFile(__dirname + "\\login.html");
      res.redirect("login.html");
 });
 
 
 // Defining the routes that are taken
-
 app.get('/login/facebook',
 	passport.authenticate('facebook'));
 
@@ -125,41 +120,27 @@ app.get('/login/facebook/return',
 
 // When the server receives connections it opens certain sockets and logs what is has done
 // When the app listens it logs where it is listening to
-
 io.on('connection', function(socket){
 
+	var user = {name: "unknown", score: 0, socketid: socket.id};
 
-    var user = {name: "unknown", score: 0, socketid: socket.id};
+	
+					// SOCKETS //
 
+	
     socket.on('Newuser', function(msg){
-		keepAlive();
         console.log("User connected : " + msg);
         user.name = msg;
         users[users.length] = user;
         io.emit('users', users);
     });
 
-	socket.on('keepAlive', function(){
-		setTimeout(keepAlive, 2000);
-		console.log("stayin' alive:" + user.name)
-	});
-
-	function keepAlive(){
-		socket.emit("keepAlive");
-	}
-
-	socket.on('disconnect', function(msg){
+	socket.on('disconnect', function(){
 		console.log('User disconnected:' + user.name);
-		user.name = msg;
-		for (var i = 0; i < users.length; i++) {
-			if(users[i]){
-				if (users[i].name === user.name){
-					users.splice(i, 1);
-					if ((game.gamerunning && users.length < 2)||socket == game.drawer){
-						stopGame();
-					}
-				}
-			}
+		var index = users.indexOf(user);
+		users.splice(index, 1);
+		if ((game.gamerunning && users.length < 2)||socket == game.drawer){
+			stopGame();
 		}
 		io.emit('users', users);
 	});
@@ -168,16 +149,16 @@ io.on('connection', function(socket){
 		console.log("Enter pressed");
 	});
 
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function(msg, property){
 		console.log("message: " + msg);
 		var newmsg = msg.substring(msg.indexOf(":") + 1);
 		if (game.gamerunning){
 			if(socket == game.drawer) {
-				socket.emit('chat message', "You can not speak whist drawing");
+				socket.emit('chat message', "You can not speak whist drawing", property);
 			} else {
 				if(newmsg.toLowerCase() === game.currentWord.toLowerCase()){
-					socket.emit('chat message', "You guessed the correct word!");
-					socket.broadcast.emit('chat message', "The word was guessed!");
+					socket.emit('chat message', "You guessed the correct word!", property);
+					socket.broadcast.emit('chat message', "The word was guessed!", property);
 					user.score += game.count;
 					io.emit('users', users);
 					io.emit('globalWordToShow', game.currentWord);
@@ -185,21 +166,15 @@ io.on('connection', function(socket){
 					setTimeout(removeCurrentWord, 5000);
 					setTimeout(newround, 6000);
 				} else {
-					io.emit('chat message', msg);
+					io.emit('chat message', msg, property);
 				}
 			}
 		} else {
-			io.emit('chat message', msg);
+			io.emit('chat message', msg, property);
 		}
-});
+}	);
 
-	var removeCurrentWord = function(){
-		var index  = game.gameWords.indexOf(game.currentWord);
-		game.gameWords.splice(index, 1);
-		console.log(game.gameWords);
-	};
-
-    socket.on('draw', function(msg){
+	socket.on('draw', function(msg){
 		if(!game.gamerunning && msg.mouse == "down" || socket == game.drawer && msg.mouse == "down"){	//Doesn't allow you to draw if you are not the drawer when the game is running
 			console.log("Draw msg received");
 			io.emit('draw', msg);
@@ -220,7 +195,6 @@ io.on('connection', function(socket){
         io.emit('users', users)
 	});
 
-
 	socket.on('newgame', function(wordSet){
 		if (users.length > 1 ){
 			game.gamerunning = true;
@@ -240,11 +214,16 @@ io.on('connection', function(socket){
 				}
 			});
 		} else {
-			socket.emit('chat message', "There must be at least two people to play a game.");
+			socket.emit('chat message', "There must be at least two people to play a game.", "server");
 		}
 	});
 
-	var newround = function(){
+
+					// GAME SEQUENCE //
+
+
+	function newround(){
+		io.emit('clear');
 		if (game.gameWords.length != 0){
 			game.drawer = socket;
 			game.round++;
@@ -255,9 +234,46 @@ io.on('connection', function(socket){
 		} else {
 			stopGame();
 		}
-	};
+	}
 
-	var stopGame = function(){
+	function timedCount(){
+		io.emit('displayTime', "Time left: " + game.count);
+		if (game.count > 0) {
+			game.count -= 1;
+			game.t = setTimeout(function () {
+				timedCount();
+			}, 1000);
+		} else {
+			io.emit('chat message', "Time is up, no one guessed the word!", "server");
+			stopCount();
+			io.emit('globalWordToShow', game.currentWord);
+			setTimeout(removeCurrentWord, 5000);
+			setTimeout(newround, 6000);
+		}
+	}
+
+	function startCount(){
+		console.log("Timer started");
+		if (!game.timerOn) {
+			game.timerOn = 1;
+			game.count = game.startCount;
+			timedCount();
+		}
+	}
+
+	function stopCount(){
+		console.log("Timer stopped");
+		clearTimeout(game.t);
+		game.timerOn = 0;
+	}
+
+	function removeCurrentWord(){
+		var index  = game.gameWords.indexOf(game.currentWord);
+		game.gameWords.splice(index, 1);
+		console.log(game.gameWords);
+	}
+
+	function stopGame(){
 		console.log("Game stopped");
 		stopCount();
 		game.drawer = null;
@@ -265,70 +281,40 @@ io.on('connection', function(socket){
 		var winners = calculateHighest();
 		if (winners.length == 1){
 			var message = "The game has ended. The winner was " + winners[0].name + " with a score of " + winners[0].score;
-			io.emit('chat message', message);
+			io.emit('chat message', message, "server");
 		} else {
 			message = "The game has ended. It was a tie of " + winners[0].score + " points, between :";
 			for (var i = 0; i < winners.length; i++){
 				message += winners[i].name + ", ";
 			}
-			io.emit('chat message', message);
+			io.emit('chat message', message, "server");
 		}
 		io.emit('resetElements');
 		game.gamerunning = false;
 		resetScores();
-	};
+	}
 
-	var resetScores = function(){
+	function resetScores(){
 		for(var i = 0; i < users.length; i++){
 			users[i].score = 0;
 			io.emit('users', users);
 		}
-	};
+	}
 
-	var calculateHighest = function(){
+	function calculateHighest(){
 		var highest = users[0].score;
 		for(var i = 0; i < users.length; i++){
-			if (users[i].score > highest)
+			if (users[i].score > highest) {
 				highest = users[i].score;
+			}
 		}
-		var winners = new Array();
+		var winners = [];
 		for(i = 0; i < users.length; i++){
 			if(users[i].score == highest){
 				winners.push(users[i]);
 			}
 		}
 		return winners;
-	};
-
-	var timedCount = function(){
-		io.emit('displayTime', "Time left: " + game.count);
-		if (game.count > 0){
-			game.count -= 1;
-			game.t = setTimeout(function(){
-				timedCount();
-			}, 1000);
-		} else {
-			io.emit('chat message', "Time is up, no one guessed the word!");
-			stopCount();
-			io.emit('globalWordToShow', game.currentWord);
-			setTimeout(removeCurrentWord, 5000);
-			setTimeout(newround, 6000);
-		}
-	};
-
-	var startCount = function(){
-		console.log("Timer started");
-		if (!game.timerOn){
-			game.timerOn = 1;
-			game.count = game.startCount;
-			timedCount();
-		}
-	};
-
-	var stopCount = function() {
-		console.log("Timer stopped");
-		clearTimeout(game.t);
-		game.timerOn = 0;
 	}
 });
 
